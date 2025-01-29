2ed9d0a52af632c1b8a8dba9ce8e1b3741e84d27
Bhushan <bhushangaykawad@gmail.com>
2025-01-29 18:27:06 +0530

    WIP

--- src/Model/Request.php
+++ src/Model/Request.php
@@ -7,105 +7,75 @@ namespace Treblle\Model;
 final class Request implements \JsonSerializable
 {
     /**
-     * The timestamp should be generated at the time the request was made and should be in format of YYYY-MM-DD hh:mm:ss.
-     */
-    private string $timestamp;
-
-    /**
-     * The real IP address of the request.
-     */
-    private string $ip;
-
-    /**
-     * A full URL of the request including query data if it has any.
-     */
-    private string $url;
-
-    /**
-     * The User Agent of the request. This can be pulled from headers but sometimes languages offer their own
-     * way of getting the User Agent and if there is one you should use that one.
-     */
-    private string $user_agent;
-
-    /**
-     * The HTTP method for the request. Should be uppercased if possible.
-     */
-    private string $method;
-
-    /**
-     * Request headers in key:value (json_encoded) format as shown below.
-     *
-     * @var array<string, string>
-     */
-    private array $headers;
-
-    /**
-     * The COMPLETE request data sent with this request. This should include any form-data values, x-www-urlencoded data,
-     * raw data or even query data. Anything that was sent as part of the request data should be shown here wrapped in a
-     * body object.
-     *
-     * @var array<int|string, mixed>
-     */
-    private array $body;
-
-    /**
-     * @todo describe
-     *
-     * @var array<int|string, mixed>
-     */
-    private array $raw;
-
-    /**
      * @param array<string, string> $headers
      * @param array<int|string, mixed> $body
      * @param array<int|string, mixed> $raw
      */
     public function __construct(
-        string $timestamp,
-        string $ip,
-        string $url,
-        string $user_agent,
-        string $method,
-        array $headers,
-        array $body,
-        array $raw
+        private string  $timestamp,
+        private string  $ip,
+        private string  $url,
+        private string  $user_agent,
+        private string  $method,
+        private array   $headers,
+        private array   $body,
+        private array   $raw,
+        private ?string $route_path = null,
     ) {
-        $this->timestamp = $timestamp;
-        $this->ip = $ip;
-        $this->url = $url;
-        $this->user_agent = $user_agent;
-        $this->method = $method;
-        $this->headers = $headers;
-        $this->body = $body;
-        $this->raw = $raw;
     }
 
+    /**
+     * The timestamp should be generated at the time the request was made and should be in format of YYYY-MM-DD hh:mm:ss.
+     */
     public function getTimestamp(): string
     {
         return $this->timestamp;
     }
 
+    /**
+     * The real IP address of the request.
+     */
     public function getIp(): string
     {
         return $this->ip;
     }
 
+    /**
+     * A full URL of the request including query data if it has any.
+     */
     public function getUrl(): string
     {
         return $this->url;
     }
 
+    /**
+     * A route path of the request excluding query data if it has any.
+     */
+    public function getRoutePath(): ?string
+    {
+        return $this->route_path;
+    }
+
+    /**
+     * The User Agent of the request. This can be pulled from headers but sometimes languages offer their own
+     * way of getting the User Agent and if there is one you should use that one.
+     */
     public function getUserAgent(): string
     {
         return $this->user_agent;
     }
 
+    /**
+     * The HTTP method for the request. Should be uppercased if possible.
+     */
     public function getMethod(): string
     {
         return $this->method;
     }
 
     /**
+     * Request headers in key:value (json_encoded) format as shown below.
+     *
      * @return array<string, string>
      */
     public function getHeaders(): array
@@ -114,6 +84,10 @@ final class Request implements \JsonSerializable
     }
 
     /**
+     * The COMPLETE request data sent with this request. This should include any form-data values, x-www-urlencoded data,
+     * raw data or even query data. Anything that was sent as part of the request data should be shown here wrapped in a
+     * body object.
+     *
      * @return array<int|string, mixed>
      */
     public function getBody(): array
@@ -122,6 +96,8 @@ final class Request implements \JsonSerializable
     }
 
     /**
+     * Raw inputs passed to php://input
+     *
      * @return array<int|string, mixed>
      */
     public function getRaw(): array
