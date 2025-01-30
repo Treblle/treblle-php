110c89a110f8805476d1518a3174498689a40d59
Bhushan <bhushangaykawad@gmail.com>
2025-01-30 15:04:23 +0530

    WIP

--- src/DataTransferObject/Response.php
+++ src/DataTransferObject/Response.php
@@ -8,52 +8,18 @@ use JsonSerializable;
 
 final class Response implements JsonSerializable
 {
-    /**
-     * Response headers in key:value (json_encoded) format as shown below.
-     *
-     * @var array<string, string>
-     */
-    private array $headers;
-
-    /**
-     * The HTTP response code.
-     * Source: https://www.restapitutorial.com/httpstatuscodes.html.
-     */
-    private int $code;
-
-    /**
-     * The response size in bytes. This represent the total JSON response size in bytes. This can be pulled from
-     * Headers but should always prefer language specific methods of getting the response size.
-     */
-    private float $size;
-
-    /**
-     * The load time of the API response in microseconds.
-     */
-    private float $load_time;
-
-    /**
-     * The COMPLETE json response as returned by the server. This should ONLY be a VALID JSON and should be wrapped
-     * inside the body object.
-     *
-     * @var array<int|string, mixed>
-     */
-    private array $body;
-
-    /**
-     * @param array<string, string> $headers
-     * @param array<int|string, mixed> $body
-     */
-    public function __construct(array $headers, int $code, float $size, float $load_time, array $body)
-    {
-        $this->headers = $headers;
-        $this->code = $code;
-        $this->size = $size;
-        $this->load_time = $load_time;
-        $this->body = $body;
+    public function __construct(
+        private array $headers,
+        private int $code,
+        private float $size,
+        private float $load_time,
+        private array $body
+    ) {
     }
 
     /**
+     * Response headers in key:value (json_encoded) format as shown below.
+     *
      * @return array<string, string>
      */
     public function getHeaders(): array
@@ -61,22 +27,36 @@ final class Response implements JsonSerializable
         return $this->headers;
     }
 
+    /**
+     * The HTTP response code.
+     * Source: https://www.restapitutorial.com/httpstatuscodes.html.
+     */
     public function getCode(): int
     {
         return $this->code;
     }
 
+    /**
+     * The response size in bytes. This represents the total JSON response size in bytes. This can be pulled from
+     * Headers but should always prefer language specific methods of getting the response size.
+     */
     public function getSize(): float
     {
         return $this->size;
     }
 
+    /**
+     * The load time of the API response in microseconds.
+     */
     public function getLoadTime(): float
     {
         return $this->load_time;
     }
 
     /**
+     * The COMPLETE json response as returned by the server. This should ONLY be a VALID JSON and should be wrapped
+     * inside the body object.
+     *
      * @return array<int|string, mixed>
      */
     public function getBody(): array
